ab8c9a84519875b964dc98a5df4872b261c2c2fc
Viktor Dakalov <atlantidu@gmail.com>
2024-12-07 23:11:54 +0500

    Continue implementing surface list and node classes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import ArchiveReader from './archive/reader';
 import TreeNode from './archive/reader/tree-node';
+import SurfaceNode from './node';
 
-export default class Surface {
+/**
+ * Each list item is tree roots
+ */
+export default class Surface extends Array<SurfaceNode> {
 
   public static fromArchiveFile(filename: string): Surface {
     const ar = new ArchiveReader(filename);
     const header = ar.readHeader();
     const nodes = ar.readTableOfContent();
+    return new this(nodes, header.roots);
   }
 
-  constructor(nodes: TreeNode[], roots: number[]) {
+  constructor(treeNodes: TreeNode[], treeRootsIndices: number[]) {
+    super(treeRootsIndices.length);
+    for (const [treeRootsIndex, treeNodeIndex] of treeRootsIndices.entries()) {
+      const resolutionLevel = treeRootsIndex > 3 ? 4 : treeRootsIndex + 1;
+      this[treeRootsIndex] = new SurfaceNode(treeNodes, treeNodeIndex, resolutionLevel);
+    }
   }
 }
--- src/node.ts
+++ src/node.ts
@@ -1,13 +1,23 @@
-import TreeNode from '../archive/reader/tree-node';
+import TreeNode from './archive/reader/tree-node';
 
 export default class SurfaceNode {
 
+  public readonly index: number;
+
   public readonly origin: TreeNode;
 
-  public readonly parent: TreeNode | undefined;
+  public readonly resolutionLevel: number;
 
-  constructor(origin: TreeNode, parent?: TreeNode) {
-    this.origin = origin;
+  public readonly parent: SurfaceNode | undefined;
+
+  public readonly children: SurfaceNode[];
+
+  constructor(treeNodes: TreeNode[], index: number, resolutionLevel: number, parent?: SurfaceNode) {
+    this.index = index;
+    this.origin = treeNodes[index];
+    this.resolutionLevel = resolutionLevel;
     this.parent = parent;
+    this.children = this.origin.children
+      .map(childIndex => new SurfaceNode(treeNodes, childIndex, this.resolutionLevel + 1, this));
   }
 }
